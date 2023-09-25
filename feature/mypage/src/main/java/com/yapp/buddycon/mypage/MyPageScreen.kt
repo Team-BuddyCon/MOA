1df6a6b0ffb255414e3dbc1070785ab03e10a3a3
ows3090 <ows3090@naver.com>
2023-09-25 16:27:48 +0900

    [Feat] #3 Add feature's mypage module

---
+++ feature/mypage/src/main/java/com/yapp/buddycon/mypage/MyPageScreen.kt
@@ -0,0 +1,12 @@
+package com.yapp.buddycon.mypage
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyPageScreen() {
+    Column {
+        Text(text = "MyPage")
+    }
+}
