46be259a1ee09caccebd9c345248fa1b316999b9
ows3090 <ows3090@naver.com>
2023-09-30 01:12:17 +0900

    [Feat] #3 Create BottomBar component

--- core/navigation/src/main/java/com/yapp/buddycon/navigation/BuddyConNavHost.kt
+++ core/navigation/src/main/java/com/yapp/buddycon/navigation/BuddyConNavHost.kt
@@ -1,12 +1,16 @@
 package com.yapp.buddycon.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.yapp.buddycon.navigation.component.BuddyConBottomBar
 import com.yapp.buddycon.navigation.gifticon.GifticonDestination
 import com.yapp.buddycon.navigation.gifticon.gifticonGraph
 import com.yapp.buddycon.navigation.map.mapGraph
@@ -19,24 +23,31 @@ const val GIFTICON_GRAPH = "gifticon_graph"
 const val MAP_GRAPH = "map_graph"
 const val MYPAGE_GRAPH = "mypage_graph"
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BuddyConNavHost(
     navHostController: NavHostController = rememberNavController()
 ) {
-    NavHost(
-        navController = navHostController,
-        startDestination = SplashDestination.route,
-        modifier = Modifier.fillMaxSize(),
-        route = ROOT_GRAPH,
-    ) {
-        composable(route = SplashDestination.route) {
-            SplashScreen {
-                navHostController.navigate(GifticonDestination.Gifticon.route)
+    Scaffold(
+        bottomBar = { BuddyConBottomBar(navHostController) },
+    ) { paddingValues ->
+        NavHost(
+            navController = navHostController,
+            startDestination = SplashDestination.route,
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            route = ROOT_GRAPH,
+        ) {
+            composable(route = SplashDestination.route) {
+                SplashScreen {
+                    navHostController.navigate(GifticonDestination.Gifticon.route)
+                }
             }
-        }
 
-        gifticonGraph(navHostController)
-        mapGraph(navHostController)
-        mypageGraph(navHostController)
+            gifticonGraph(navHostController)
+            mapGraph(navHostController)
+            mypageGraph(navHostController)
+        }
     }
 }
