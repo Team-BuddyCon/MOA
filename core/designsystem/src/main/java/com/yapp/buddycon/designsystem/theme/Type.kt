2a2b285c741bb4a8c50bb1b15d2548310488fa5a
ows3090 <ows3090@naver.com>
2023-10-10 01:53:00 +0900

    [Feat] #5 BuddyConTheme에 적용할 Typography 정의

--- core/designsystem/src/main/java/com/yapp/buddycon/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/yapp/buddycon/designsystem/theme/Type.kt
@@ -1,34 +1,119 @@
 package com.yapp.buddycon.designsystem.theme
 
+import android.speech.tts.TextToSpeech
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 30.sp,
+        lineHeight = 42.sp,
+        letterSpacing = (-0.02).em
+    ),
+    titleMedium = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = (33.6).sp,
+        letterSpacing = (-0.02).em
+    ),
+    titleSmall = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = (25.2).sp,
+        letterSpacing = (-0.02).em
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = pretendardFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = (22.4).sp,
+        letterSpacing = (-0.02).em
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = pretendardFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = (19.6).sp,
+        letterSpacing = (-0.02).em
+    ),
+    bodySmall = TextStyle(
+        fontFamily = pretendardFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 10.sp,
+        lineHeight = (16.8).sp,
+        letterSpacing = (-0.02).em
     )
-    */
 )
+
+val Typography.title01: TextStyle
+    @Composable get() = titleMedium
+
+val Typography.title02: TextStyle
+    @Composable get() = titleMedium.copy(
+        fontSize = 22.sp,
+        lineHeight = (30.8).sp
+    )
+
+val Typography.title03: TextStyle
+    @Composable get() = titleMedium.copy(
+        fontSize = 18.sp,
+        lineHeight = (25.2).sp
+    )
+
+val Typography.title05: TextStyle
+    @Composable get() = titleSmall
+
+val Typography.subTitle: TextStyle
+    @Composable get() = titleMedium.copy(
+        fontSize = 16.sp,
+        lineHeight = (22.4).sp
+    )
+
+val Typography.body01: TextStyle
+    @Composable get() = bodyLarge
+
+val Typography.body02: TextStyle
+    @Composable get() = bodyMedium
+
+val Typography.body03: TextStyle
+    @Composable get() = bodyLarge.copy(
+        fontSize = 14.sp,
+        lineHeight = (19.6).sp
+    )
+
+val Typography.body04: TextStyle
+    @Composable get() = bodyMedium.copy(
+        fontSize = 12.sp,
+        lineHeight = (14.32).sp
+    )
+
+val Typography.body05: TextStyle
+    @Composable get() = bodyLarge.copy(
+        fontSize = 12.sp,
+        lineHeight = (16.8).sp
+    )
+
+val Typography.body06: TextStyle
+    @Composable get() = bodySmall
