bac637b8a2a7526e242e9ba16b4239d491c494a7
ows3090 <ows3090@naver.com>
2023-10-16 01:18:45 +0900

    [Feat] #5 MaterialTheme 확장 방식으로 폰트 스타일 추가

--- core/designsystem/src/main/java/com/yapp/buddycon/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/yapp/buddycon/designsystem/theme/Theme.kt
@@ -8,10 +8,15 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -61,9 +66,115 @@ fun BuddyConTheme(
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
+    val buddyConTypography = BuddyConTypography(
+        largeTitle = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 30.sp,
+            lineHeight = 42.sp,
+            letterSpacing = (-0.02).em
+        ),
+        title01 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp,
+            lineHeight = (33.6).sp,
+            letterSpacing = (-0.02).em
+        ),
+        title02 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 22.sp,
+            lineHeight = (30.8).sp,
+            letterSpacing = (-0.02).em
+        ),
+        title03 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+            lineHeight = (25.2).sp,
+            letterSpacing = (-0.02).em
+        ),
+        title05 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+            lineHeight = (25.2).sp,
+            letterSpacing = (-0.02).em
+        ),
+        subTitle = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            lineHeight = (22.4).sp,
+            letterSpacing = (-0.02).em
+        ),
+        body01 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            lineHeight = (22.4).sp,
+            letterSpacing = (-0.02).em
+        ),
+        body02 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            lineHeight = (19.6).sp,
+            letterSpacing = (-0.02).em
+        ),
+        body03 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+            lineHeight = (19.6).sp,
+            letterSpacing = (-0.02).em
+        ),
+        body04 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Bold,
+            fontSize = 12.sp,
+            lineHeight = (16.8).sp,
+            letterSpacing = (-0.02).em
+        ),
+        body05 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp,
+            lineHeight = (16.8).sp,
+            letterSpacing = (-0.02).em
+        ),
+        body06 = TextStyle(
+            color = Grey90,
+            fontFamily = pretendardFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 10.sp,
+            lineHeight = (16.8).sp,
+            letterSpacing = (-0.02).em
+        )
     )
+
+    CompositionLocalProvider(LocalBuddyConTypography provides buddyConTypography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content
+        )
+    }
+}
+
+object BuddyConTheme {
+    val typography: BuddyConTypography
+        @Composable
+        get() = LocalBuddyConTypography.current
 }
--- core/designsystem/src/main/java/com/yapp/buddycon/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/yapp/buddycon/designsystem/theme/Type.kt
@@ -6,115 +6,48 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.em
-import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    titleLarge = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Medium,
-        fontSize = 30.sp,
-        lineHeight = 42.sp,
-        letterSpacing = (-0.02).em
-    ),
-    titleMedium = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Bold,
-        fontSize = 24.sp,
-        lineHeight = (33.6).sp,
-        letterSpacing = (-0.02).em
-    ),
-    titleSmall = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 18.sp,
-        lineHeight = (25.2).sp,
-        letterSpacing = (-0.02).em
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = (22.4).sp,
-        letterSpacing = (-0.02).em
-    ),
-    bodyMedium = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Bold,
-        fontSize = 14.sp,
-        lineHeight = (19.6).sp,
-        letterSpacing = (-0.02).em
-    ),
-    bodySmall = TextStyle(
-        fontFamily = pretendardFamily,
-        fontWeight = FontWeight.Medium,
-        fontSize = 10.sp,
-        lineHeight = (16.8).sp,
-        letterSpacing = (-0.02).em
-    )
+@Immutable
+data class BuddyConTypography(
+    val largeTitle: TextStyle,
+    val title01: TextStyle,
+    val title02: TextStyle,
+    val title03: TextStyle,
+    val title05: TextStyle,
+    val subTitle: TextStyle,
+    val body01: TextStyle,
+    val body02: TextStyle,
+    val body03: TextStyle,
+    val body04: TextStyle,
+    val body05: TextStyle,
+    val body06: TextStyle
 )
 
-val Typography.title01: TextStyle
-    @Composable get() = titleMedium
-
-val Typography.title02: TextStyle
-    @Composable get() = titleMedium.copy(
-        fontSize = 22.sp,
-        lineHeight = (30.8).sp
+val LocalBuddyConTypography = staticCompositionLocalOf {
+    BuddyConTypography(
+        largeTitle = TextStyle.Default,
+        title01 = TextStyle.Default,
+        title02 = TextStyle.Default,
+        title03 = TextStyle.Default,
+        title05 = TextStyle.Default,
+        subTitle = TextStyle.Default,
+        body01 = TextStyle.Default,
+        body02 = TextStyle.Default,
+        body03 = TextStyle.Default,
+        body04 = TextStyle.Default,
+        body05 = TextStyle.Default,
+        body06 = TextStyle.Default
     )
-
-val Typography.title03: TextStyle
-    @Composable get() = titleMedium.copy(
-        fontSize = 18.sp,
-        lineHeight = (25.2).sp
-    )
-
-val Typography.title05: TextStyle
-    @Composable get() = titleSmall
-
-val Typography.subTitle: TextStyle
-    @Composable get() = titleMedium.copy(
-        fontSize = 16.sp,
-        lineHeight = (22.4).sp
-    )
-
-val Typography.body01: TextStyle
-    @Composable get() = bodyLarge
-
-val Typography.body02: TextStyle
-    @Composable get() = bodyMedium
-
-val Typography.body03: TextStyle
-    @Composable get() = bodyLarge.copy(
-        fontSize = 14.sp,
-        lineHeight = (19.6).sp
-    )
-
-val Typography.body04: TextStyle
-    @Composable get() = bodyMedium.copy(
-        fontSize = 12.sp,
-        lineHeight = (14.32).sp
-    )
-
-val Typography.body05: TextStyle
-    @Composable get() = bodyLarge.copy(
-        fontSize = 12.sp,
-        lineHeight = (16.8).sp
-    )
-
-val Typography.body06: TextStyle
-    @Composable get() = bodySmall
+}
 
 @Preview(showBackground = true, widthDp = 540)
 @Composable
@@ -129,168 +62,144 @@ private fun TypographyPreview() {
                 Text(
                     text = "Large Title",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.titleLarge
+                    style = BuddyConTheme.typography.largeTitle
                 )
                 Text(
                     text = "Pretendard / Medium / 30",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.titleLarge
+                    style = BuddyConTheme.typography.largeTitle
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Title 01",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title01
+                    style = BuddyConTheme.typography.title01
                 )
                 Text(
                     text = "Pretendard / Bold / 24",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title01
+                    style = BuddyConTheme.typography.title01
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Title 02",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title02
+                    style = BuddyConTheme.typography.title02
                 )
                 Text(
                     text = "Pretendard / Bold / 22",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title02
+                    style = BuddyConTheme.typography.title02
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Title 03",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title03
+                    style = BuddyConTheme.typography.title03
                 )
                 Text(
                     text = "Pretendard / Bold / 22",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title03
+                    style = BuddyConTheme.typography.title03
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Title 05",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title05
+                    style = BuddyConTheme.typography.title05
                 )
                 Text(
                     text = "Pretendard / Regular / 18",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.title05
+                    style = BuddyConTheme.typography.title05
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Sub Title",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.subTitle
+                    style = BuddyConTheme.typography.subTitle
                 )
                 Text(
                     text = "Pretendard / Bold / 16",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.subTitle
+                    style = BuddyConTheme.typography.subTitle
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Body 01",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body01
+                    style = BuddyConTheme.typography.body01
                 )
                 Text(
                     text = "Pretendard / Regular / 16",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body01
+                    style = BuddyConTheme.typography.body01
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Body 02",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body02
+                    style = BuddyConTheme.typography.body02
                 )
                 Text(
                     text = "Pretendard / Bold / 14",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body02
+                    style = BuddyConTheme.typography.body02
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Body 03",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body03
+                    style = BuddyConTheme.typography.body03
                 )
                 Text(
                     text = "Pretendard / Regular / 14",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body03
+                    style = BuddyConTheme.typography.body03
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Body 04",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body04
+                    style = BuddyConTheme.typography.body04
                 )
                 Text(
                     text = "Pretendard / Bold / 12",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body04
+                    style = BuddyConTheme.typography.body04
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Body 05",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body05
+                    style = BuddyConTheme.typography.body05
                 )
                 Text(
                     text = "Pretendard / Regular / 12",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body05
+                    style = BuddyConTheme.typography.body05
                 )
             }
             Row(Modifier.padding(top = 20.dp)) {
                 Text(
                     text = "Body 06",
                     modifier = Modifier.width(142.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body06
+                    style = BuddyConTheme.typography.body06
                 )
                 Text(
                     text = "Pretendard / Medium / 10",
                     modifier = Modifier.padding(start = 54.dp),
-                    color = Color(0xFF2D2D2D),
-                    style = MaterialTheme.typography.body06
+                    style = BuddyConTheme.typography.body06
                 )
             }
         }
