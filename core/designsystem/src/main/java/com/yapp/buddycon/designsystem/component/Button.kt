b201813c5d32bef8a0d47abfe243fd982e806a1c
JJAE WON <55980680+ashwon12@users.noreply.github.com>
2023-10-29 02:26:10 +0900

    [UI] #8 기본 버튼 Componet 수정 및 추가

--- core/designsystem/src/main/java/com/yapp/buddycon/designsystem/component/Button.kt
+++ core/designsystem/src/main/java/com/yapp/buddycon/designsystem/component/Button.kt
@@ -2,7 +2,7 @@ package com.yapp.buddycon.designsystem.component
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -10,30 +10,92 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.yapp.buddycon.designsystem.R
+import com.yapp.buddycon.designsystem.theme.BuddyConTheme
+import com.yapp.buddycon.designsystem.theme.Grey30
+import com.yapp.buddycon.designsystem.theme.Grey40
+import com.yapp.buddycon.designsystem.theme.Grey60
+import com.yapp.buddycon.designsystem.theme.Pink100
 
 @Composable
-fun BottomFullButton(
-    onButtonClick: () -> Unit,
-    buttonBackgroundColor: Color = Color(0xFFFF4E6E),
-    buttonTextColor: Color = Color.White,
-    buttonText: String = stringResource(id = R.string.button_default_name)
+fun MainButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    bgColors: Color = Pink100,
+    onClick: () -> Unit,
+) {
+    val textColor : Color = if (bgColors == Grey30) Grey60 else White
+    BuddyConBasicButton(
+        onClick = onClick,
+        buttonText = text,
+        modifier = modifier,
+        buttonBackgroundColor = bgColors,
+        buttonTextColor = textColor)
+}
+
+@Composable
+fun DisabledButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+){
+    BuddyConBasicButton(
+        onClick = { onClick },
+        buttonBackgroundColor = Grey40,
+        buttonText = text,
+        buttonTextColor = Grey60,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun BuddyConBasicButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    buttonBackgroundColor: Color,
+    buttonTextColor: Color = White,
+    buttonText: String = stringResource(id = R.string.button_default_name),
 ) {
     Button(
-        onClick = { onButtonClick() },
-        modifier = Modifier
+        onClick = { onClick() },
+        modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-        shape = RoundedCornerShape(12.dp),
-        colors = ButtonDefaults.buttonColors(
-            containerColor = buttonBackgroundColor,
-            contentColor = buttonTextColor
-        ),
-        contentPadding = PaddingValues(vertical = 16.dp)
+            .height(54.dp),
+            shape = RoundedCornerShape(12.dp), colors = ButtonDefaults.buttonColors(
+            containerColor = buttonBackgroundColor
+        ), contentPadding = PaddingValues(vertical = 16.dp)
     ) {
-        // todo - 버튼 text 폰트 및 사이즈 설정하기 -
-        Text(text = buttonText)
+        Text(
+            text = buttonText, style = BuddyConTheme.typography.subTitle, color = buttonTextColor
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun MainPinkButtonPreview() {
+    BuddyConTheme {
+        MainButton(text = "버튼"){}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun GreyButtonPreview() {
+    BuddyConTheme {
+        MainButton(text = "버튼", bgColors = Grey30){}
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DisabledButtonPreview() {
+    BuddyConTheme {
+        DisabledButton(text = "버튼"){}
     }
 }
--- feature/mypage/src/main/java/com/yapp/buddycon/mypage/MyPageScreen.kt
+++ feature/mypage/src/main/java/com/yapp/buddycon/mypage/MyPageScreen.kt
@@ -3,7 +3,6 @@ package com.yapp.buddycon.mypage
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.yapp.buddycon.designsystem.component.BottomFullButton
 
 @Composable
 fun MyPageScreen() {
@@ -14,6 +13,6 @@ fun MyPageScreen() {
         //      - 디자인 시스템 요소들이 앱에서 어떻게 표출되는지 볼 수 있는 별도의 화면 구성하기
         //      - 버디콘 개발자 모드를 별도로 만들고 이곳에서 볼 수 있도록?
         //      - 현재는 마이페이지 탭에서 임시로 볼 수 있도록 배치
-        BottomFullButton(onButtonClick = {})
+//        BottomFullButton(onButtonClick = {})
     }
 }
