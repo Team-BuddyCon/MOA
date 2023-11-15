55ed6e747d2e7d11e588d5cd8e6f44ec2a25fc08
ows3090 <ows3090@naver.com>
2023-11-16 01:22:42 +0900

    [build] #26 ktlint 에러 수정

--- core/designsystem/src/main/java/com/yapp/buddycon/designsystem/component/appbar/BaseAppBar.kt
+++ core/designsystem/src/main/java/com/yapp/buddycon/designsystem/component/appbar/BaseAppBar.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.yapp.buddycon.designsystem.R
 import com.yapp.buddycon.designsystem.theme.BuddyConTheme
-import com.yapp.buddycon.designsystem.theme.Grey90
 import com.yapp.buddycon.designsystem.theme.Paddings
 import com.yapp.buddycon.designsystem.theme.Pink100
 
