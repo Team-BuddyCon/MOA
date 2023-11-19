0717eb1e12af24bf78ed54a1740f936bb6d4e7ec
ows3090 <ows3090@naver.com>
2023-11-19 16:48:07 +0900

    [chore] #27 GifticonModel expirationTime 변수 타입 수정 및 Date 반환 메소드 추가

--- domain/src/main/java/com/yapp/buddycon/domain/model/gifticon/GifticonModel.kt
+++ domain/src/main/java/com/yapp/buddycon/domain/model/gifticon/GifticonModel.kt
@@ -1,10 +1,16 @@
 package com.yapp.buddycon.domain.model.gifticon
 
 import com.yapp.buddycon.domain.model.type.GifticonCategory
+import java.text.SimpleDateFormat
+
+private const val DATE_FORMAT = "yyyy.MM.dd"
 
 data class GifticonModel(
     val imageUrl: String = "",
     val category: GifticonCategory = GifticonCategory.ETC,
     val name: String = "",
-    val expirationDate: String = ""
-)
+    val expirationTime: Long = 0L
+) {
+    @Suppress("SimpleDateFormat")
+    fun toExpirationDate(): String = SimpleDateFormat(DATE_FORMAT).format(expirationTime)
+}
