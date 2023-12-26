53721115152b505ddfe8a5a51215ae23e41a415a
ows3090 <ows3090@naver.com>
2023-12-27 02:49:53 +0900

    [UI] #43 지도 화면에 필요한 바텀 시트 구현

--- core/designsystem/src/main/java/com/yapp/buddycon/designsystem/component/modal/GifticonInfoModalSheet.kt
+++ core/designsystem/src/main/java/com/yapp/buddycon/designsystem/component/modal/GifticonInfoModalSheet.kt
@@ -1,24 +1,16 @@
 package com.yapp.buddycon.designsystem.component.modal
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,64 +27,51 @@ private val GifticonInfoModalSheetHeight = 167.dp
 private val GifticonInfoModalSheetRadius = 24.dp
 private val GifticonInfoModalHorizontalPadding = 15.dp
 private val GifticonInfoModalItemIconSize = 64.dp
-private val GifticonInfoModalDescription = "GifticonInfoModal"
+private const val GifticonInfoModalDescription = "GifticonInfoModal"
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GifticonInfoModalSheet(
-    sheetState: SheetState = rememberModalBottomSheetState(),
+fun GifticonInfoModalSheetContent(
+    modifier: Modifier = Modifier,
     countOfUsableGifticon: Int,
-    countOfImminetGifticon: Int,
-    onDismiss: () -> Unit
+    countOfImminetGifticon: Int
 ) {
-    ModalBottomSheet(
-        onDismissRequest = onDismiss,
-        modifier = Modifier
+    Row(
+        modifier = modifier
+            .background(BuddyConTheme.colors.background)
             .fillMaxWidth()
-            .height(GifticonInfoModalSheetHeight),
-        sheetState = sheetState,
-        shape = RoundedCornerShape(
-            topStart = GifticonInfoModalSheetRadius,
-            topEnd = GifticonInfoModalSheetRadius
-        ),
-        containerColor = BuddyConTheme.colors.modalColor
+            .padding(horizontal = GifticonInfoModalHorizontalPadding)
+            .wrapContentHeight(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = GifticonInfoModalHorizontalPadding)
-                .wrapContentHeight(),
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.weight(1f)
         ) {
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = stringResource(R.string.modal_sheet_gifticon),
-                    style = BuddyConTheme.typography.subTitle
-                )
-                Row(verticalAlignment = Alignment.Bottom) {
-                    Text(
-                        text = "${countOfUsableGifticon}개",
-                        style = BuddyConTheme.typography.title01
-                    )
-                    Text(
-                        text = String.format(
-                            stringResource(R.string.modal_sheet_imminet_gifticon),
-                            countOfImminetGifticon
-                        ),
-                        modifier = Modifier.padding(start = Paddings.medium, bottom = Paddings.small),
-                        style = BuddyConTheme.typography.body04.copy(color = Pink100)
-                    )
-                }
-            }
-            Icon(
-                painter = painterResource(R.drawable.ic_gifticon),
-                contentDescription = GifticonInfoModalDescription,
-                modifier = Modifier.size(GifticonInfoModalItemIconSize),
-                tint = Color.Unspecified
+            Text(
+                text = stringResource(R.string.modal_sheet_gifticon),
+                style = BuddyConTheme.typography.subTitle
             )
+            Row(verticalAlignment = Alignment.Bottom) {
+                Text(
+                    text = "${countOfUsableGifticon}개",
+                    style = BuddyConTheme.typography.title01
+                )
+                Text(
+                    text = String.format(
+                        stringResource(R.string.modal_sheet_imminet_gifticon),
+                        countOfImminetGifticon
+                    ),
+                    modifier = Modifier.padding(start = Paddings.medium, bottom = Paddings.small),
+                    style = BuddyConTheme.typography.body04.copy(color = Pink100)
+                )
+            }
         }
+        Icon(
+            painter = painterResource(R.drawable.ic_gifticon),
+            contentDescription = GifticonInfoModalDescription,
+            modifier = Modifier.size(GifticonInfoModalItemIconSize),
+            tint = Color.Unspecified
+        )
     }
 }
 
@@ -100,14 +79,10 @@ fun GifticonInfoModalSheet(
 @Preview
 @Composable
 private fun GifticonInfoModalSheetPreview() {
-    var isShowModal by remember { mutableStateOf(true) }
     BuddyConTheme {
-        if (isShowModal) {
-            GifticonInfoModalSheet(
-                countOfUsableGifticon = 12,
-                countOfImminetGifticon = 1,
-                onDismiss = { isShowModal = false }
-            )
-        }
+        GifticonInfoModalSheetContent(
+            countOfUsableGifticon = 12,
+            countOfImminetGifticon = 1
+        )
     }
 }
--- feature/map/src/main/java/com/yapp/buddycon/map/MapScreen.kt
+++ feature/map/src/main/java/com/yapp/buddycon/map/MapScreen.kt
@@ -1,12 +1,47 @@
 package com.yapp.buddycon.map
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.yapp.buddycon.designsystem.R
+import com.yapp.buddycon.designsystem.component.appbar.TopAppBarWithNotification
+import com.yapp.buddycon.designsystem.component.modal.GifticonInfoModalSheetContent
+import com.yapp.buddycon.designsystem.theme.BuddyConTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapScreen() {
-    Column {
-        Text(text = "Map")
+    BottomSheetScaffold(
+        sheetContent = {
+            GifticonInfoModalSheetContent(
+                modifier = Modifier.padding(bottom = 11.dp),
+                countOfUsableGifticon = 12,
+                countOfImminetGifticon = 1
+            )
+        },
+        scaffoldState = rememberBottomSheetScaffoldState(
+            bottomSheetState = SheetState(
+                skipPartiallyExpanded = false,
+                initialValue = SheetValue.Expanded,
+                skipHiddenState = true
+            )
+        ),
+        sheetShape = RoundedCornerShape(topStart = 24.dp, topEnd = 24.dp),
+        sheetContainerColor = BuddyConTheme.colors.background,
+        sheetShadowElevation = 33.dp,
+        topBar = {
+            TopAppBarWithNotification(
+                title = stringResource(R.string.map)
+            )
+        }
+    ) {
     }
 }
