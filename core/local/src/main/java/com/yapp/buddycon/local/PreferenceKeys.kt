220b2e602e081ae2532d369e5abd422ce5f0e212
ows3090 <ows3090@naver.com>
2024-02-12 17:46:56 +0900

    [feat] #46 TokenRepository에 액세스토큰, 리프레쉬토큰 저장하는 로직 추가

--- core/local/src/main/java/com/yapp/buddycon/local/PreferenceKeys.kt
+++ core/local/src/main/java/com/yapp/buddycon/local/PreferenceKeys.kt
@@ -1,9 +1,13 @@
 package com.yapp.buddycon.local
 
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 
 object PreferenceKeys {
     val IS_FIRST_INSTALLATION = booleanPreferencesKey("IS_FIRST_INSTALLATION")
     val NICKNAME = stringPreferencesKey("NICKNAME")
+    val ACCESS_TOKEN = stringPreferencesKey("ACCESS_TOKEN")
+    val REFRESH_TOKEN = stringPreferencesKey("REFRESH_TOKEN")
+    val ACCESS_TOKEN_EXPIRES_IN = longPreferencesKey("ACCESS_TOKEN_EXPIRES_IN")
 }
